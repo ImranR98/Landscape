938b5a0b0e2b2c5e3954a021e4acd78933e67255
Imran Remtulla <imranr98@pm.me>
2024-08-17 00:07:59 -0400

    Progress (Authelia works but in non-prod mode) + namespace changes

--- setup-funcs.sh
+++ setup-funcs.sh
@@ -3,11 +3,14 @@
 # Ensure K8s and Helm are already set up
 # Ensure frpc is already set up
 
+init() {
+    kubectl create namespace production
+}
+
 installTraefik() {
     helm repo add traefik https://helm.traefik.io/traefik
     helm repo update
-    kubectl create namespace traefik
-    helm install --namespace=traefik traefik traefik/traefik --values=traefik/values.yaml
+    helm install --namespace=production traefik traefik/traefik --values=traefik/values.yaml
     kubectl apply -f default-headers.yaml
     kubectl apply -f traefik/secret-dashboard.yaml
     kubectl apply -f traefik/middleware.yaml
@@ -17,11 +20,18 @@ installTraefik() {
 installCertManager() {
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
-    kubectl create namespace cert-manager
     CERT_MANAGER_VERSION="$(curl -s https://api.github.com/repos/cert-manager/cert-manager/releases/latest | grep -oE 'tag_name": .+' | awk -F '"' '{print $3}')"
-    helm install cert-manager jetstack/cert-manager --namespace cert-manager --values=cert-manager/values.yaml --version "$CERT_MANAGER_VERSION" --set crds.enabled=true
+    helm install cert-manager jetstack/cert-manager --production cert-manager --values=cert-manager/values.yaml --version "$CERT_MANAGER_VERSION" --set crds.enabled=true
     kubectl apply -f cert-manager/issuers/secret-cf-token.yaml
     kubectl apply -f cert-manager/issuers/
     kubectl apply -f cert-manager/certificates/staging/
     kubectl apply -f cert-manager/certificates/production/
+}
+
+installAuthelia() {
+    helm repo add authelia https://charts.authelia.com
+    helm repo update
+    kubectl create -n authelia secret generic authelia-users --from-file=authelia/users-database.yaml
+    AUTHELIA_VERSION="$(curl https://charts.authelia.com/ | grep '\--version' | awk '{print $NF}')"
+    helm install authelia authelia/authelia --version "$AUTHELIA_VERSION" --values authelia/values.yaml --namespace production
 }
