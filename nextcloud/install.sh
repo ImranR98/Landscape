0108f5eb3dcc843a05cc3f2e6a41d0e785d008d1
Imran Remtulla <imranr98@pm.me>
2024-09-03 18:58:58 -0400

    Switched NextCloud via manifest (no Helm)

--- nextcloud/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-helm install --namespace production nextcloud nextcloud/nextcloud -f "$HERE"/values.yaml
--- nextcloud/prep.sh
+++ nextcloud/prep.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-helm repo add nextcloud https://nextcloud.github.io/helm/
-helm repo update
 mkdir -p "$HERE"/../state/nextcloud/db
-mkdir -p "$HERE"/../state/nextcloud/data/root
-mkdir -p "$HERE"/../state/nextcloud/data/html
-mkdir -p "$HERE"/../state/nextcloud/data/data
-mkdir -p "$HERE"/../state/nextcloud/data/config
-mkdir -p "$HERE"/../state/nextcloud/data/custom_apps
-mkdir -p "$HERE"/../state/nextcloud/data/tmp
-mkdir -p "$HERE"/../state/nextcloud/data/themes
-sudo chown -R root "$HERE"/../state/nextcloud/data
+mkdir -p "$HERE"/../state/nextcloud/data
