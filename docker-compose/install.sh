4d1b974b27525b513590719dda0bd45321103d09
Imran Remtulla <imranr98@pm.me>
2024-09-04 20:21:14 -0400

    Added Syncthing to docker-compose + Docker svc install bugfixes

--- docker-compose/install.sh
+++ docker-compose/install.sh
@@ -5,5 +5,5 @@ docker run --rm imranrdev/logtfy k8s >"$HERE"/prepLogtfy.sh
 docker run --rm imranrdev/logtfy role >"$HERE"/role.yaml
 bash "$HERE"/prepLogtfy.sh production
 echo "KUBE_API_SERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')" >"$HERE"/.env
-awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_SCRIPT_DIR", SCRIPT_DIR); print}' "$HERE"/docker-compose.service | sudo tee /etc/systemd/system/docker-compose.service
+awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_here", SCRIPT_DIR); print}' "$HERE"/docker-compose.service | sudo tee /etc/systemd/system/docker-compose.service
 sudo systemctl enable --now docker-compose.service
--- frpc/install.sh
+++ frpc/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_SCRIPT_DIR", SCRIPT_DIR); print}' "$HERE"/frpc.service | sudo tee /etc/systemd/system/frpc.service
+awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_here", SCRIPT_DIR); print}' "$HERE"/frpc.service | sudo tee /etc/systemd/system/frpc.service
 sudo systemctl enable --now frpc.service
