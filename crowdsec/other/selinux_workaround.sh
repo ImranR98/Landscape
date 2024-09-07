06a891940ec16400c01eb55db932b67b7450e12e
Imran Remtulla <imranr98@pm.me>
2024-09-07 01:01:14 -0400

    Updated CrowdSec bugfix

--- crowdsec/other/selinux_workaround.sh
+++ crowdsec/other/selinux_workaround.sh
@@ -7,20 +7,22 @@ POLICY_FILE="/tmp/allow_logreader_to_watch_logs.te"
 MODULE_FILE="/tmp/allow_logreader_to_watch_logs.mod"
 PACKAGE_FILE="/tmp/allow_logreader_to_watch_logs.pp"
 
-cat <<EOF > $POLICY_FILE # Create the policy file
+cat <<EOF >$POLICY_FILE # Create the policy file
 module allow_logreader_to_watch_logs 1.0;
 
 require {
     class file { watch watch_reads };
+    class dir { watch };
     type container_logreader_t;
     type container_log_t;
 };
 
 allow container_logreader_t container_log_t:file { watch watch_reads };
+allow container_logreader_t container_log_t:dir { watch };
 EOF
-checkmodule -M -m -o "$MODULE_FILE" "$POLICY_FILE"    # Compile the policy file into a module
-semodule_package -m $MODULE_FILE -o $PACKAGE_FILE # Package the module into a policy package
-sudo semodule -i $PACKAGE_FILE                         # Install the policy package into SELinux
+checkmodule -M -m -o "$MODULE_FILE" "$POLICY_FILE" # Compile the policy file into a module
+semodule_package -m $MODULE_FILE -o $PACKAGE_FILE  # Package the module into a policy package
+sudo semodule -i $PACKAGE_FILE                     # Install the policy package into SELinux
 
 # Clean up
 rm -f $POLICY_FILE $MODULE_FILE $PACKAGE_FILE
