a1fb7943af0f1786d29f0e2b4e08ea17d6abe2d5
Imran Remtulla <imranr98@pm.me>
2024-09-07 00:28:42 -0400

    Crowdsec bugfix

---
+++ crowdsec/other/selinux_workaround.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+# https://github.com/crowdsecurity/helm-charts/issues/190
+
+# Define the policy file and module names
+POLICY_FILE="/tmp/allow_logreader_to_watch_logs.te"
+MODULE_FILE="/tmp/allow_logreader_to_watch_logs.mod"
+PACKAGE_FILE="/tmp/allow_logreader_to_watch_logs.pp"
+
+cat <<EOF > $POLICY_FILE # Create the policy file
+module allow_logreader_to_watch_logs 1.0;
+
+require {
+    class file { watch watch_reads };
+    type container_logreader_t;
+    type container_log_t;
+};
+
+allow container_logreader_t container_log_t:file { watch watch_reads };
+EOF
+checkmodule -M -m -o "$MODULE_FILE" "$POLICY_FILE"    # Compile the policy file into a module
+semodule_package -m $MODULE_FILE -o $PACKAGE_FILE # Package the module into a policy package
+sudo semodule -i $PACKAGE_FILE                         # Install the policy package into SELinux
+
+# Clean up
+rm -f $POLICY_FILE $MODULE_FILE $PACKAGE_FILE
+
+# Apply the required label to the deployment
+kubectl patch -n production daemonset crowdsec-agent --type='strategic' -p '{
+  "spec": {
+    "template": {
+      "spec": {
+        "securityContext": {
+          "seLinuxOptions": {
+            "type": "container_logreader_t"
+          }
+        }
+      }
+    }
+  }
+}'
--- crowdsec/update.sh
+++ crowdsec/update.sh
@@ -2,22 +2,7 @@
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 helm repo update
 helm upgrade --namespace production crowdsec crowdsec/crowdsec --values "$HERE"/values.yaml
+bash "$HERE"/other/selinux_workaround.sh
 kubectl -n production rollout restart deployment crowdsec-lapi 
 kubectl -n production rollout restart daemonset crowdsec-agent
-
-# TODO: TEMP: https://github.com/crowdsecurity/helm-charts/issues/190
-kubectl patch -n production daemonset crowdsec-agent --type='strategic' -p '{
-  "spec": {
-    "template": {
-      "spec": {
-        "securityContext": {
-          "seLinuxOptions": {
-            "type": "container_logreader_t"
-          }
-        }
-      }
-    }
-  }
-}'
-
 echo "NOTE: The Helm chart was updated, but there is no guarantee that the chart is in sync with the latest service release."
