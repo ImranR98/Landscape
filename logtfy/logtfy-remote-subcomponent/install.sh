50ea4d09ed23e96d73edd94d8db044741c875217
Imran Remtulla <imranr98@pm.me>
2024-09-07 17:31:51 -0400

    Bugfixes

--- logtfy/logtfy-remote-subcomponent/install.sh
+++ logtfy/logtfy-remote-subcomponent/install.sh
@@ -10,6 +10,6 @@ SSH_STRING="$SSH_USER@$SSH_HOST"
 
 jq '.moduleCustomization = [.moduleCustomization[] | select(.module == "ssh_logins") | .loggerArg = "ssh"]' "$HERE"/../logtfy.json > "$HERE"/files/logtfy.json
 
-generateComposeService logtfy | "$HERE"/files/logtfy.service
+generateComposeService logtfy > "$HERE"/files/logtfy.service
 rsync -r "$HERE"/files/ "$SSH_STRING":~/logtfy/
 ssh -A -t "$SSH_STRING" "bash '/home/$SSH_USER/logtfy/install.sh'"
