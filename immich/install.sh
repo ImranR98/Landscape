2c4f8ae8030d56a1b1bc3e51711bf8275ba1dcd0
Imran Remtulla <imranr98@pm.me>
2024-09-07 23:50:23 -0400

    Bugfix

--- immich/install.sh
+++ immich/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-helm install --namespace production immich immich/immich <(cat "$HERE"/values.yaml | envsubst)
+helm install --namespace production immich immich/immich --values <(cat "$HERE"/values.yaml | envsubst)
--- immich/update.sh
+++ immich/update.sh
@@ -2,6 +2,6 @@
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 helm repo update
-helm upgrade --namespace production immich immich/immich <(cat "$HERE"/values.yaml | envsubst)
+helm upgrade --namespace production immich immich/immich --values <(cat "$HERE"/values.yaml | envsubst)
 kubectl -n production rollout restart deployment immich-server immich-machine-learning
 kubectl -n production rollout restart statefulset immich-postgresql immich-redis-master
