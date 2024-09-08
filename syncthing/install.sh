f656577aeb25295133b54f3388c80bdba556c7f0
Imran Remtulla <imranr98@pm.me>
2024-09-07 23:48:06 -0400

    Bugfix

--- syncthing/install.sh
+++ syncthing/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$HERE"/../helpers.sh
+
 docker pull syncthing/syncthing:latest
 generateComposeService syncthing | awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_here", SCRIPT_DIR); print}' | sudo tee /etc/systemd/system/syncthing.service.temp
 awk -v MY_UID="$UID" '{gsub("User=1000", MY_UID); print}' /etc/systemd/system/syncthing.service.temp | sudo tee /etc/systemd/system/syncthing.service
