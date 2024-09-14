bf98a9409341712c14276d26bd014714f390ac4c
Imran Remtulla <imranr98@pm.me>
2024-09-14 09:31:45 -0500

    Run FRPC in K8s + tweaks

--- cluster-base/files/prep_cluster.sh
+++ cluster-base/files/prep_cluster.sh
@@ -4,4 +4,5 @@ HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 kubectl create namespace production
 kubectl label nodes "$(hostname | tr "[:upper:]" "[:lower:]")" main-storage=true # Assume the control plane node is also the main-storage node
+kubectl label nodes "$(hostname | tr "[:upper:]" "[:lower:]")" entrypoint=true # Assume the control plane node is also the FRPC node
 mkdir -p "$HERE"/state
--- frpc/update.sh
+++ frpc/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-bash "$HERE"/install.sh
+bash "$HERE"/prep.sh
