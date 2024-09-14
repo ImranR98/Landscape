940297bf1eb7910bc8a6b0e31cb8ef347ba1537e
Imran Remtulla <imranr98@pm.me>
2024-09-14 00:30:35 -0500

    Bugfix

--- traefik/prep.sh
+++ traefik/prep.sh
@@ -3,6 +3,6 @@ set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
-mkdir -p "$STATE_DIR"/traefik-metrics/prometheus
-mkdir -p "$STATE_DIR"/traefik-metrics/grafana/data
-mkdir -p "$STATE_DIR"/traefik-metrics/grafana/provisioning
+mkdir -p "$STATE_DIR"/prometheus-grafana/prometheus
+mkdir -p "$STATE_DIR"/prometheus-grafana/grafana/data
+mkdir -p "$STATE_DIR"/prometheus-grafana/grafana/provisioning
