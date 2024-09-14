279ecf830759b6ca4cd811ba87d9514428c92ce1
Imran Remtulla <imranr98@pm.me>
2024-09-14 10:17:20 -0500

    Run Syncthing in K8s

--- syncthing/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-source "$HERE"/../helpers.sh
-
-docker pull syncthing/syncthing:latest
-generateComposeService syncthing | awk -v SCRIPT_DIR="$HERE" '{gsub("path_to_here", SCRIPT_DIR); print}' | sudo tee /etc/systemd/system/syncthing.service
-sudo systemctl enable syncthing.service
-sudo systemctl restart syncthing.service
--- syncthing/update.sh
+++ syncthing/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-bash "$HERE"/install.sh
+kubectl -n production rollout restart deployment syncthing
