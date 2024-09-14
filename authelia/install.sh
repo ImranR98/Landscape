a2f60595b8617ebc4e15b4d27b999ab8aa70ce1b
Imran Remtulla <imranr98@pm.me>
2024-09-14 10:31:04 -0500

    Added a comment

--- authelia/install.sh
+++ authelia/install.sh
@@ -10,3 +10,5 @@ sed '/# IGNORE INITIALLY$/ s/^/# /' "$HERE"/values.yaml >"$TEMP_FILE"
 helm install authelia authelia/authelia --values <(cat "$TEMP_FILE" | envsubst) --namespace production
 echo "NOTE: Some lines in authelia/values.yaml were commented out according to the \"IGNORE INITIALLY\" annotation.
       Run authelia/upgrade.sh to apply the original values.yaml (after ensuring it is okay to do so)."
+echo 'NOTE: To get notification.txt from inside the container, run this:
+      kubectl -n production exec --stdin --tty "$(kubectl -n production get pod | grep authelia | grep -v redis | grep -v postgres | awk '{print \$1}')" -- cat /config/notification.txt'
