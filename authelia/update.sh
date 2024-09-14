b7c76987501173fdb743c4558597cc5940f79595
Imran Remtulla <imranr98@pm.me>
2024-09-14 02:05:55 -0500

    Bug

--- authelia/update.sh
+++ authelia/update.sh
@@ -2,7 +2,7 @@
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 helm repo update
-helm upgrade authelia authelia/authelia --values --values <(cat "$HERE"/values.yaml | envsubst) --namespace production
+helm upgrade authelia authelia/authelia --values <(cat "$HERE"/values.yaml | envsubst) --namespace production
 kubectl -n production rollout restart daemonset authelia
 kubectl -n production rollout restart statefulset authelia-postgresql authelia-redis-master
 echo "NOTE: The Helm chart was updated, but there is no guarantee that the chart is in sync with the latest service release."
