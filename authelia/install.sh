65562898ec217454280589dcdef60029afc7d4b0
Imran Remtulla <imranr98@pm.me>
2024-09-14 12:06:16 -0500

    Script improvement

--- authelia/install.sh
+++ authelia/install.sh
@@ -9,6 +9,8 @@ kubectl create -n production secret generic authelia-users --from-file="$TEMP_SE
 sed '/# IGNORE INITIALLY$/ s/^/# /' "$HERE"/values.yaml >"$TEMP_FILE"
 helm install authelia authelia/authelia --values <(cat "$TEMP_FILE" | envsubst) --namespace production
 echo "NOTE: Some lines in authelia/values.yaml were commented out according to the \"IGNORE INITIALLY\" annotation.
-      Run authelia/upgrade.sh to apply the original values.yaml (after ensuring it is okay to do so)."
+      The update script will uncomment those lines, so ensure it is okay to do so before running it.
+      Specifically, you should check that the services at these domains are setup appropriately:
+$(cat "$HERE"/values.yaml | envsubst | grep -Eo 'domain: .+# IGNORE INITIALLY$' | awk '{print "      - ", $2}')"
 echo 'NOTE: To get notification.txt from inside the container, run this:
       kubectl -n production exec --stdin --tty "$(kubectl -n production get pod | grep authelia | grep -v redis | grep -v postgres | awk '{print \$1}')" -- cat /config/notification.txt'
