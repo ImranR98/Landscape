8befd5c0563623a8fa160ed5fff12fbd5750c8c4
Imran Remtulla <imranr98@pm.me>
2024-09-14 23:27:04 -0500

    Install script message fixes

--- ntfy/post_install.sh
+++ ntfy/post_install.sh
@@ -7,7 +7,7 @@ SERVICES_TOPIC="${SERVICES_USER}_*"
 printLine -
 echo 'NOTE: You may need to create a user, generate a token, and replace it in VARS.sh. Example of helpful commands (run in the container):
 
-kubectl -n production exec --stdin --tty "$(kubectl -n production get pod | grep authelia | grep -v redis | grep -v postgres | awk '{print \$1}')" -- cat /config/notification.txt'
+kubectl -n production exec --stdin --tty "$(kubectl -n production get pod | grep ntfy | awk '"'"'{print $1}'"'"')" -- sh'
 
 echo "
 ntfy user add $USER
