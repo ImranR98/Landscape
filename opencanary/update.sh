51ec1d8ee4bf992534c44c0554e206311048656c
Imran Remtulla <imranr98@pm.me>
2024-09-19 19:15:58 -0500

    Bugfix

--- opencanary/update.sh
+++ opencanary/update.sh
@@ -2,3 +2,4 @@
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 kubectl -n production rollout restart deployment opencanary
+kubectl -n production delete pod "$(kubectl -n production get pod | grep opencanary | awk '{print $1}' | tail -1)"
