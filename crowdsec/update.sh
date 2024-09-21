eafb69ba655b3be191022afe2aad38035f3a8cd2
Imran Remtulla <imranr98@pm.me>
2024-09-21 19:40:20 -0400

    Added a message

--- crowdsec/update.sh
+++ crowdsec/update.sh
@@ -6,4 +6,5 @@ helm upgrade --namespace production crowdsec crowdsec/crowdsec --values <(cat "$
 bash "$HERE"/other/selinux_workaround.sh
 kubectl -n production rollout restart deployment crowdsec-lapi 
 kubectl -n production rollout restart daemonset crowdsec-agent
+echo "Reminder: Ensure CrowdSec works by simulating an attack: 'nikto -h <URL>'"
 echo "NOTE: The Helm chart was updated, but there is no guarantee that the chart is in sync with the latest service release."
