124abc30d136fe7c7321400ce1eaae1168d941a7
Imran Remtulla <imranr98@pm.me>
2024-09-21 19:25:58 -0400

    Traefik trusted IPs bugfix

--- fixed.VARS.sh
+++ fixed.VARS.sh
@@ -7,7 +7,7 @@ export MAIN_NODE_HOSTNAME_LOWERCASE="${MAIN_NODE_HOSTNAME,,}"
 export PROXY_NODE_HOSTNAME_LOWERCASE="${PROXY_NODE_HOSTNAME,,}"
 export HELPERS_PATH="$HERE/helpers.sh"
 
-export TRAEFIK_TRUSTED_IP_RANGE="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | tail -1)"
+export TRAEFIK_TRUSTED_IP_RANGE="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | sed 's/.*/"&"/' | tr '\n' ',' | sed 's/,$//')"
 
 export SERVICES_TLS_NAME="$(echo "$SERVICES_TOP_DOMAIN" | sed 's/\./-/g')"
 
