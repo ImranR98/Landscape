9ac70ff157edb69024b3e79a2ff8fa88912eed95
Imran Remtulla <imranr98@pm.me>
2024-09-27 16:05:34 -0400

    Mosquitto update script bugfix

--- mosquitto/update.sh
+++ mosquitto/update.sh
@@ -2,3 +2,4 @@
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 kubectl -n production rollout restart deployment mosquitto
+kubectl -n production delete pod "$(kubectl -n production get pod | grep mosquitto | awk '{print $1}' | tail -1)"
