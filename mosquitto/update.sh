bc14ed948e5afc94e694a8abe46dad92ea38412c
Imran Remtulla <imranr98@pm.me>
2024-10-11 17:50:21 -0400

    Minor update script optimizations

--- mosquitto/update.sh
+++ mosquitto/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-kubectl -n production rollout restart deployment mosquitto
-kubectl -n production delete pod "$(kubectl -n production get pod | grep mosquitto | awk '{print $1}' | tail -1)"
+kubectl -n production delete pod "$(kubectl -n production get pod | grep mosquitto | awk '{print $1}' | tail -1)" # 'rollout restart' causes conflict btn. instances
--- opencanary/update.sh
+++ opencanary/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-kubectl -n production rollout restart deployment opencanary
-kubectl -n production delete pod "$(kubectl -n production get pod | grep opencanary | awk '{print $1}' | tail -1)"
+ubectl -n production delete pod "$(kubectl -n production get pod | grep opencanary | awk '{print $1}' | tail -1)" # 'rollout restart' causes conflict btn. instances
