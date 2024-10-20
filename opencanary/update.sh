408a243ba348f007160dee1bfffb01d0e387e096
Imran Remtulla <imranr98@pm.me>
2024-10-20 16:34:03 -0400

    Typo

--- opencanary/update.sh
+++ opencanary/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ubectl -n production delete pod "$(kubectl -n production get pod | grep opencanary | awk '{print $1}' | tail -1)" # 'rollout restart' causes conflict btn. instances
+kubectl -n production delete pod "$(kubectl -n production get pod | grep opencanary | awk '{print $1}' | tail -1)" # 'rollout restart' causes conflict btn. instances
