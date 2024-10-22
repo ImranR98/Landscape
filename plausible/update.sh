c28709b1f5ebbf137d11d5538d1b2b9dd6ae196b
Imran Remtulla <imranr98@pm.me>
2024-10-22 13:30:53 -0400

    Bugfix in 'getImageDigest' and plausible/update.sh

--- plausible/update.sh
+++ plausible/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-kubectl -n production rollout restart deployment plausible-db plausible-clickhouse plausible
+kubectl -n production rollout restart deployment plausible-clickhouse plausible
+deletePodsByGrep plausible-db
