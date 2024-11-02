538ae2ace439bf800cdbf0ec3deaa78d15d15e4d
Imran Remtulla <imranr98@pm.me>
2024-11-02 19:25:13 -0400

    Progress on Docker version

--- docker/prep.sh
+++ docker/prep.sh
@@ -24,20 +24,27 @@ for dir in $(grep -Eo '\$STATE_DIR[^:]+:' "$HERE"/docker-compose.yaml | awk -F: 
     mkdir -p "$STATE_DIR/$(echo $dir | tail -c +12)"
 done
 
-
-# $STATE_DIR/logtfy/config.json
-# $STATE_DIR/authelia/config/configuration.yaml
-# $STATE_DIR/authelia/config/users_database.yaml
-# $STATE_DIR/traefik/acme.json
-# $STATE_DIR/traefik/dynamic-configuration.yaml
-# $STATE_DIR/traefik/mtls/cacert.pem
-# $STATE_DIR/traefik/mtls/cakey.pem
-# $STATE_DIR/prometheus/prometheus.yaml
-# $STATE_DIR/crowdsec/acquis.yaml
-# $STATE_DIR/crowdsec/profiles.yaml
-# $STATE_DIR/crowdsec/notifications-http.yaml
-# $STATE_DIR/opencanary/opencanary.json
-# $STATE_DIR/filebrowser/filebrowser.db
-# $STATE_DIR/filebrowser/filebrowser.json
-# $STATE_DIR/ntfy/etc/server.yml
-# $STATE_DIR/mosquitto/config/mosquitto.conf
+# Generate required config files
+cat "$HERE"/config-templates/logtfy.json | envsubst >"$STATE_DIR"/logtfy/config.json
+cat "$HERE"/config-templates/authelia.config.yaml | envsubst >"$STATE_DIR"/authelia/config/configuration.yaml
+echo "$$AUTHELIA_USERS_DATABASE" >"$STATE_DIR"/authelia/config/users_database.yaml
+if [ ! -f "$STATE_DIR"/traefik/acme.json ]; then
+    echo '[]' >"$STATE_DIR"/traefik/acme.json
+fi
+cat "$HERE"/config-templates/traefik.dynamic-configuration.yaml | envsubst >"$STATE_DIR"/traefik/dynamic-configuration.yaml
+cat "$HERE"/config-templates/prometheus.yaml | envsubst >"$STATE_DIR"/prometheus/prometheus.yaml
+cat "$HERE"/config-templates/crowdsec.acquis.yaml | envsubst >"$STATE_DIR"/crowdsec/acquis.yaml
+cat "$HERE"/config-templates/crowdsec.notifications-http.yaml | envsubst >"$STATE_DIR"/crowdsec/notifications-http.yaml
+cat "$HERE"/config-templates/crowdsec.profiles.yaml | envsubst >"$STATE_DIR"/crowdsec/profiles.yaml
+cat "$HERE"/config-templates/opencanary.json | envsubst >"$STATE_DIR"/opencanary/opencanary.json
+touch "$STATE_DIR"/filebrowser/filebrowser.db
+cat "$HERE"/config-templates/filebrowser.json | envsubst >"$STATE_DIR"/filebrowser/filebrowser.json
+cat "$HERE"/config-templates/ntfy.server.yml | envsubst >"$STATE_DIR"/ntfy/server.yml
+cat "$HERE"/config-templates/mosquitto.conf | envsubst >"$STATE_DIR"/mosquitto/config/mosquitto.conf
+echo "$MOSQUITTO_PRIVATE_KEY" >"$STATE_DIR"/mosquitto/config/private_key.pem
+echo "$MOSQUITTO_CERTIFICATE" >"$STATE_DIR"/mosquitto/config/certificate.pem
+echo "$MOSQUITTO_CREDENTIALS" >"$STATE_DIR"/mosquitto/config/password_file
+if [ ! -f "$STATE_DIR"/traefik/mtls/cacert.pem ] || [ ! -f "$STATE_DIR"/traefik/mtls/cakey.pem ]; then
+    openssl genrsa -out "$STATE_DIR"/traefik/mtls/cakey.pem 4096
+    openssl req -new -x509 -key "$STATE_DIR"/traefik/mtls/cakey.pem -out "$STATE_DIR"/traefik/mtls/cacert.pem
+fi
