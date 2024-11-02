e6d6b413de8aaf9df5237f875f6a75348f7c7a5f
Imran Remtulla <imranr98@pm.me>
2024-11-01 21:03:10 -0400

    Fix prev commit

--- logtfy/update.sh
+++ logtfy/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$HELPERS_PATH"
 bash "$HERE"/prep.sh
-kubectl -n production rollout restart deployment logtfy
+deletePodsByGrep logtfy
