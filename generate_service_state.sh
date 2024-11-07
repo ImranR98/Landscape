845e29b878441ec85ed849d1786bc4c0f957a468
Imran Remtulla <contact@imranr.dev>
2024-11-06 19:03:34 -0500

    Bugfixes

--- generate_service_state.sh
+++ generate_service_state.sh
@@ -3,8 +3,11 @@ set -e
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd;)"/init_vars.sh
 
-# Ensure state dirs exist
-for dir in $(grep -Eo '\$STATE_DIR[^:]+:' "$(here)"/landscape.docker-compose.yaml | awk -F: '{print $1}' | grep -E '/[^(/|.)]+$'); do
+# Ensure state and other required dirs exist
+grep -Eo '\$MAIN_PARENT_DIR[^:]+:' "$(here)"/landscape.docker-compose.yaml | awk -F: '{print $1}' | grep -E '/[^(/|.)]+$' | while read dir; do
+    mkdir -p "$MAIN_PARENT_DIR/$(echo $dir | tail -c +18)"
+done
+grep -Eo '\$STATE_DIR[^:]+:' "$(here)"/landscape.docker-compose.yaml | awk -F: '{print $1}' | grep -E '/[^(/|.)]+$' | while read dir; do
     mkdir -p "$STATE_DIR/$(echo $dir | tail -c +12)"
 done
 mkdir -p "$STATE_DIR"/logtfy
@@ -28,12 +31,12 @@ cat "$(here)"/files/opencanary.json | envsubst >"$STATE_DIR"/opencanary/opencana
 touch "$STATE_DIR"/filebrowser/filebrowser.db
 cat "$(here)"/files/filebrowser.json | envsubst >"$STATE_DIR"/filebrowser/filebrowser.json
 cat "$(here)"/files/ntfy.server.yml | envsubst >"$STATE_DIR"/ntfy/server.yml
-cat "$(here)"/files/mosquitto.conf | envsubst >"$STATE_DIR"/mosquitto/config/mosquitto.conf
 cp "$(here)"/files/hwaccel.ml.yml "$STATE_DIR"/immich
 cp "$(here)"/files/hwaccel.transcoding.yml "$STATE_DIR"/immich
-echo "$MOSQUITTO_PRIVATE_KEY" >"$STATE_DIR"/mosquitto/config/private_key.pem
-echo "$MOSQUITTO_CERTIFICATE" >"$STATE_DIR"/mosquitto/config/certificate.pem
-echo "$MOSQUITTO_CREDENTIALS" >"$STATE_DIR"/mosquitto/config/password_file
+cat "$(here)"/files/mosquitto.conf | envsubst | sudo dd status=none of="$STATE_DIR"/mosquitto/config/mosquitto.conf
+echo "$MOSQUITTO_PRIVATE_KEY" | sudo dd status=none of="$STATE_DIR"/mosquitto/config/private_key.pem
+echo "$MOSQUITTO_CERTIFICATE" | sudo dd status=none of="$STATE_DIR"/mosquitto/config/certificate.pem
+echo "$MOSQUITTO_CREDENTIALS" | sudo dd status=none of="$STATE_DIR"/mosquitto/config/password_file
 if [ ! -f "$STATE_DIR"/traefik/mtls/cacert.pem ] || [ ! -f "$STATE_DIR"/traefik/mtls/cakey.pem ]; then
     openssl genrsa -out "$STATE_DIR"/traefik/mtls/cakey.pem 4096
     openssl req -new -x509 -key "$STATE_DIR"/traefik/mtls/cakey.pem -out "$STATE_DIR"/traefik/mtls/cacert.pem
