d845a276de48dcbea965d6a1a965e1987dce56da
Imran Remtulla <contact@imranr.dev>
2024-11-08 19:00:33 -0500

    Added FRPS

--- install_remote.sh
+++ install_remote.sh
@@ -4,17 +4,7 @@ set -e
 HERE_M3U8="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd;)"
 source "$HERE_M3U8"/prep_env.sh
 
-# Install FRPS
-syncRemoteEnvFileIfUndefined "$PROXY_SSH_STRING" "$PROXY_HOME/frps/frps-tokens.txt" "$MAIN_NODE_HOSTNAME_LOWERCASE" "$(echo $RANDOM | sha512sum | awk '{print $1}')$(echo $RANDOM | sha512sum | awk '{print $1}')" "$HERE_M3U8"/files/frps-tokens.txt
-generateComposeService frps 1000 > "$HERE_M3U8"/files/frps.service
-scp "$HERE_M3U8"/files/.gitignore "$PROXY_SSH_STRING":~/frps/.gitignore
-scp "$HERE_M3U8"/files/frps-tokens.txt "$PROXY_SSH_STRING":~/frps/frps-tokens.txt
-scp "$HERE_M3U8"/files/frps.docker-compose.yaml "$PROXY_SSH_STRING":~/frps/frps.docker-compose.yaml
-scp "$HERE_M3U8"/files/frps.service "$PROXY_SSH_STRING":~/frps/frps.service
-scp "$HERE_M3U8"/files/frps.install.sh "$PROXY_SSH_STRING":~/frps/install.sh
-scp "$HERE_M3U8"/files/openTCPPort.sh "$PROXY_SSH_STRING":~/frps/openTCPPort.sh
-rm "$HERE_M3U8"/files/frps.service
-rm "$HERE_M3U8"/files/frps-tokens.txt
+# Install Docker
 ssh -A -t "$PROXY_SSH_STRING" bash -l <<-EOF
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -30,4 +20,28 @@ sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker \$USER
 EOF
+
+# # Install FRPS
+syncRemoteEnvFileIfUndefined "$PROXY_SSH_STRING" "$PROXY_HOME/frps/frps-tokens.txt" "$MAIN_NODE_HOSTNAME_LOWERCASE" "$(echo $RANDOM | sha512sum | awk '{print $1}')$(echo $RANDOM | sha512sum | awk '{print $1}')" "$HERE_M3U8"/files/frps-tokens.txt
+generateComposeService frps 1000 > "$HERE_M3U8"/files/frps.service
+scp "$HERE_M3U8"/files/.gitignore "$PROXY_SSH_STRING":~/frps/.gitignore
+scp "$HERE_M3U8"/files/frps-tokens.txt "$PROXY_SSH_STRING":~/frps/frps-tokens.txt
+scp "$HERE_M3U8"/files/frps.docker-compose.yaml "$PROXY_SSH_STRING":~/frps/frps.docker-compose.yaml
+scp "$HERE_M3U8"/files/frps.service "$PROXY_SSH_STRING":~/frps/frps.service
+scp "$HERE_M3U8"/files/frps.install.sh "$PROXY_SSH_STRING":~/frps/install.sh
+scp "$HERE_M3U8"/files/openTCPPort.sh "$PROXY_SSH_STRING":~/frps/openTCPPort.sh
+rm "$HERE_M3U8"/files/frps.service
+rm "$HERE_M3U8"/files/frps-tokens.txt
 ssh -A -t "$PROXY_SSH_STRING" "bash '$PROXY_HOME/frps/install.sh'"
+
+# Install strelaysrv
+generateComposeService strelaysrv 1000 > "$HERE_M3U8"/files/strelaysrv.service
+cat "$HERE_M3U8"/files/strelaysrv.docker-compose.template.yaml | envsubst > "$HERE_M3U8"/files/strelaysrv.docker-compose.yaml
+ssh -A -t "$PROXY_SSH_STRING" "mkdir -p '$PROXY_HOME/strelaysrv'"
+scp "$HERE_M3U8"/files/.gitignore "$PROXY_SSH_STRING":~/strelaysrv/.gitignore
+scp "$HERE_M3U8"/files/strelaysrv.install.sh "$PROXY_SSH_STRING":~/strelaysrv/install.sh
+scp "$HERE_M3U8"/files/strelaysrv.service "$PROXY_SSH_STRING":~/strelaysrv/strelaysrv.service
+scp "$HERE_M3U8"/files/strelaysrv.docker-compose.yaml "$PROXY_SSH_STRING":~/strelaysrv/strelaysrv.docker-compose.yaml
+rm "$HERE_M3U8"/files/strelaysrv.service
+rm "$HERE_M3U8"/files/strelaysrv.docker-compose.yaml
+ssh -A -t "$PROXY_SSH_STRING" "bash '$PROXY_HOME/strelaysrv/install.sh'"
