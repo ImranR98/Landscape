151a00b35c8a85e43c92b42825bad5bc09b13d76
Imran Remtulla <imranr98@pm.me>
2025-01-24 20:18:58 -0500

    Make Geoblock countries configurable

--- template.VARS.sh
+++ template.VARS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# NOTE: Indentation must be retained in multiline variables
+
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export MAIN_NODE_HOSTNAME="controlplane"
@@ -21,20 +23,20 @@ export IMMICH_OAUTH_CLIENT_SECRET="abc" # echo $RANDOM | sha256sum | awk '{print
 export IMMICH_DB_PASSWORD="abc"         # echo $RANDOM | sha256sum | awk '{print $1}'
 
 export AUTHELIA_USERS_DATABASE="users:
-      admin:
-        disabled: false
-        displayname: \"Admin\"
-        password: \"\$argon2id\$v=19\$m=65536,t=3,p=abc\" # yamllint disable-line rule:line-length
-        email: $DOMAIN_OWNER_EMAIL
-        groups:
-          - admins"                     # docker run -it authelia/authelia:latest authelia crypto hash generate argon2
+  admin:
+    disabled: false
+    displayname: \"Admin\"
+    password: \"\$argon2id\$v=19\$m=65536,t=3,p=abc\" # yamllint disable-line rule:line-length
+    email: $DOMAIN_OWNER_EMAIL
+    groups:
+      - admins"                         # docker run -it authelia/authelia:latest authelia crypto hash generate argon2
 export AUTHELIA_DB_ENCRYPTION_KEY="abc" # echo $RANDOM | sha256sum | awk '{print $1}'
 export AUTHELIA_SESSION_SECRET="abc"    # echo $RANDOM | sha256sum | awk '{print $1}'
 export AUTHELIA_JWT_SECRET="abc"        # echo $RANDOM | sha256sum | awk '{print $1}'
-export AUTHELIA_OIDC_HMAC_SECRET="abc" # echo $RANDOM | sha256sum | awk '{print $1}'
+export AUTHELIA_OIDC_HMAC_SECRET="abc"  # echo $RANDOM | sha256sum | awk '{print $1}'
 export AUTHELIA_JWKS_KEY="-----BEGIN PRIVATE KEY-----
-              abc
-              -----END PRIVATE KEY-----"                  #openssl genrsa -out private.pem 2048 # openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+          abc
+          -----END PRIVATE KEY-----"                      #openssl genrsa -out private.pem 2048 # openssl rsa -in private.pem -outform PEM -pubout -out public.pem
 export AUTHELIA_IMMICH_CLIENT_SECRET='$pbkdf2-sha512$abc' # docker run authelia/authelia:latest authelia crypto hash generate pbkdf2 --variant sha512 --random --random.length 72 --random.charset rfc3986 "$IMMICH_OAUTH_CLIENT_SECRET"
 
 export OPENCANARY_NTFY_OFFICIAL_TOPIC="abc123"
@@ -42,11 +44,11 @@ export OPENCANARY_NTFY_OFFICIAL_TOPIC="abc123"
 export MONEROD_RPC_LOGIN="admin:abc"
 
 export MOSQUITTO_PRIVATE_KEY="-----BEGIN PRIVATE KEY-----
-    abc
-    -----END PRIVATE KEY-----" # openssl req  -nodes -new -x509  -keyout private_key.pem -out certificate.pem -subj "/C=CA/ST=Toronto/L=Toronto/O=$SERVICES_TOP_DOMAIN/OU=Main/CN=$SERVICES_TOP_DOMAIN"
+abc
+-----END PRIVATE KEY-----" # openssl req  -nodes -new -x509  -keyout private_key.pem -out certificate.pem -subj "/C=CA/ST=Toronto/L=Toronto/O=$SERVICES_TOP_DOMAIN/OU=Main/CN=$SERVICES_TOP_DOMAIN"
 export MOSQUITTO_CERTIFICATE="-----BEGIN CERTIFICATE-----
-    abc
-    -----END CERTIFICATE-----"
+abc
+-----END CERTIFICATE-----"
 export MOSQUITTO_CREDENTIALS='admin:abc'
 
 export PLAUSIBLE_SECRET_KEY="abc"     # openssl rand -base64 48
@@ -54,8 +56,16 @@ export PLAUSIBLE_TOTP_VAULT_KEY="abc" # openssl rand -base64 32
 
 export STRELAYSRV_PROVIDED_BY_TEXT="example.org"
 
-export WEBDAV_HTPASSWD='user:abc' # touch htpasswd && htpasswd -B htpasswd user
+export WEBDAV_HTPASSWD='user:abc'   # touch htpasswd && htpasswd -B htpasswd user
 
 export PIXELNTFY_TOPIC_SUFFIX='abc' # echo $RANDOM | sha256sum | awk '{print $1}' | head -c 32
 
 export LOGTFY_SYNCTHING_EXCLUDED_DEVICE_IDS=''
+
+export GEOBLOCK_CONFIG_SUBSET='
+          blackListMode: false
+          countries:
+            - CA
+            - CN
+            - CU
+'
