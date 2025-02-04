d127f5bf254c1caf12636dd81c35c16870d69ba3
Imran Remtulla <imranr98@pm.me>
2025-02-03 21:39:02 -0500

    Bugfix

--- simple_restart.sh
+++ simple_restart.sh
@@ -8,6 +8,9 @@ if [ -n "$2" ]; then
     PROJECT="$2"
 fi
 cat "$HERE_F00D"/"$PROJECT".docker-compose.yaml | envsubst >"$STATE_DIR"/"$PROJECT".docker-compose.yaml
+if [ -f "$HERE_LX1A"/private.docker-compose.yaml ]; then
+    mergeComposeFiles "$STATE_DIR"/landscape.docker-compose.yaml "$HERE_LX1A"/private.docker-compose.yaml
+fi
 if [ -n "$1" ]; then
     docker compose -p "$PROJECT" -f ./state/"$PROJECT".docker-compose.yaml down "$1"
     docker compose -p "$PROJECT" -f ./state/"$PROJECT".docker-compose.yaml up -d "$1"
