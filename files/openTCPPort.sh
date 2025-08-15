3a767406e272b03d9af0c26361cb180de512b756
Imran Remtulla <contact@imranr.dev>
2025-08-15 15:32:07 -0400

    Updated comment

--- files/openTCPPort.sh
+++ files/openTCPPort.sh
@@ -7,16 +7,16 @@ else
     ufw allow "$1"
 fi
 
-# 22/tcp SSH
-# 7000/tcp FRP
-# 7000/udp FRP
-# 8887/tcp PREBOOT-SSH
-# 22067/tcp STRELAYSRV
-# 22070/tcp STRELAYSRV
-# Plus the output of 'grep 'remote_port' files/frpc.template.ini | awk '{print $3 "/tcp"}' | sort | uniq' on the main server. As of now it is:
-# 10000/tcp
-# 18080/tcp
-# 18089/tcp
-# 443/tcp
-# 80/tcp
-# 8888/tcp
+# 22/TCP SSH
+# 7000/TCP FRP
+# 7000/UDP FRP
+# 80/TCP HTTP
+# 443/TCP HTTPS
+# 8887/TCP PREBOOT-SSH
+# 18080/TCP MONEROD
+# 18089/TCP MONEROD
+# 10000/UDP JITSI
+# 22067/TCP STRELAYSRV
+# 22070/TCP STRELAYSRV
+
+# DO NOT OPEN 8888/TCP (DIRECT-CORE-SSH)
