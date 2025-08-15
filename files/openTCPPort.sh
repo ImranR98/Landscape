1068e00bafe0f63de3dc1bd179f0b37919c07784
Imran Remtulla <contact@imranr.dev>
2025-08-15 15:08:59 -0400

    Support ufw + better port list reminder

--- files/openTCPPort.sh
+++ files/openTCPPort.sh
@@ -1,15 +1,22 @@
-sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport $1 -j ACCEPT
-if [ -n "$(which netfilter-persistent)" ]; then
-    sudo netfilter-persistent save
+if ! which ufw 2>&1 >/dev/null; then
+    sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport $1 -j ACCEPT
+    if [ -n "$(which netfilter-persistent)" ]; then
+        sudo netfilter-persistent save
+    fi
+else
+    ufw allow "$1"
 fi
 
-# 22/TCP SSH
-# 7000/TCP FRP
-# 7000/UDP FRP
-# 80/TCP HTTP
-# 443/TCP HTTPS
-# 8887/TCP PREBOOT-SSH
-# 18080/TCP MONEROD
-# 18089/TCP MONEROD
-# 22067/TCP STRELAYSRV
-# 22070/TCP STRELAYSRV
+# 22/tcp SSH
+# 7000/tcp FRP
+# 7000/udp FRP
+# 8887/tcp PREBOOT-SSH
+# 22067/tcp STRELAYSRV
+# 22070/tcp STRELAYSRV
+# Plus the output of 'grep 'remote_port' files/frpc.template.ini | awk '{print $3 "/tcp"}' | sort | uniq' on the main server. As of now it is:
+# 10000/tcp
+# 18080/tcp
+# 18089/tcp
+# 443/tcp
+# 80/tcp
+# 8888/tcp
